1b0d0333df1442b6d5af1cc91581a67eb6a8bb3b
Mark Hsu <mark50404@gmail.com>
2021-11-25 05:46:33 +0800

    完成頁面骨架

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    title = 'Shop Demo'
+    return render(request, 'index.html', locals())
+
+
+def shop(request, category):
+    title = str.upper(category) + ' | Shop Demo'
+    item_id = 1
+    name = 'T-shirt'
+    price = 100
+    intro = 'This is ...'
+    inventory = 5
+    img = '/static/media/' + category + '/' + str(item_id) + '.png'
+    return render(request, 'shop.html', locals())
--- shopping_site_demo/urls.py
+++ shopping_site_demo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import index, shop
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('shop/<str:category>/', shop),
 ]
