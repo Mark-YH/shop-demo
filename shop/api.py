92975aec8056f870c96750426d005cfa4737d958
Mark Hsu <mark50404@gmail.com>
2021-12-10 19:40:56 +0800

    新增 category validation

--- shop/api.py
+++ shop/api.py
@@ -14,15 +14,15 @@ import PIL.Image as PImage
 import copy
 
 
-def save_images(request, item, response_msg):
+def save_images(request, item):
     def is_valid_image(f):
         try:
-            PImage.open(f)
-            return True
+            with PImage.open(f):
+                return True
         except IOError:
             return False
 
-    response_msg.update({'images': []})
+    response_message = {'images': []}
     files = request.FILES.getlist('images')
 
     PImage.init()
@@ -32,12 +32,25 @@ def save_images(request, item, response_msg):
         if not (content_type[0] == 'image' and
                 content_type[1].upper() in PImage.EXTENSION.values() and
                 is_valid_image(f)):
-            response_msg['images'].append({file.name: 'Invalid file type.'})
+            response_message['images'].append({file.name: 'Invalid file type.'})
         else:
             img = Image.objects.create(item=item, image=bytes(file.read()))
             img.save()
-            response_msg['images'].append({file.name: 'Successful uploaded.'})
-    return response_msg
+            response_message['images'].append({file.name: 'Successful uploaded.'})
+    return response_message
+
+
+def check_category(request):
+    response_message = {}
+    category = None
+    try:
+        category = Category.objects.get(name=request.data['category'])
+    except Category.DoesNotExist:
+        response_message.update({'category': ['Category not found.']})
+    except KeyError:
+        response_message.update({'category': ['This field is required.']})
+
+    return category, response_message
 
 
 class ItemList(APIView):
@@ -49,12 +62,18 @@ class ItemList(APIView):
     def post(self, request):
         if not request.user.is_staff:
             return Response({'Permission': 'Denied'}, status.HTTP_401_UNAUTHORIZED)
+
         item_serializer = ItemSerializer(data=request.data)
+        category, response_message = check_category(request)
+
         if item_serializer.is_valid():
-            item = item_serializer.save(category=Category.objects.get(name=request.data['category']))
-            response_message = save_images(request, item, dict(item_serializer.data.items()))
+            if category is not None:
+                item = item_serializer.save(category=category)
+                response_message.update(dict(item_serializer.data))
+                response_message.update(save_images(request, item))
             return Response(response_message, status=status.HTTP_201_CREATED)
-        return Response(item_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response_message.update(item_serializer.errors)
+        return Response(response_message, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ItemSpecific(APIView):
@@ -75,8 +94,15 @@ class ItemSpecific(APIView):
         item = self.get_object(item_id)
         serializer = ItemSerializer(item, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save(category=Category.objects.get(name=request.data['category']))
-            response_message = save_images(request, item, dict(serializer.data.items()))
+            category, msg = check_category(request)
+            if category is None:
+                serializer.save()
+            else:
+                serializer.save(category=category)
+            response_message = dict(serializer.data)
+            response_message.update(save_images(request, item))
+            if msg:
+                response_message.update(msg)
             return Response(response_message)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
